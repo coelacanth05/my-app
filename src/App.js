87b2bfb875900399bdde85c9647b9753e3535786
kai <coelacanth2505@gmail.com>
2021-11-03 20:41:45 +0900

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          よろしくお願いします by シーラカンス
+          よろしくお願いします by シーラカン
         </p>
         <a
           className="App-link"
